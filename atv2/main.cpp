570a3c67c340e78384cd676cf1ec0c90ad6efcf8
brenoaguirres <bennyaguirres1998@gmail.com>
2023-09-04 14:35:40 -0300

    First Commit

---
+++ atv2/main.cpp
@@ -0,0 +1,16 @@
+// C++20 Masterclass
+//-------------------------------
+// Pointer to char
+//-------------------------------
+// Student: Breno Freitas Aguirres
+// Date: 04/09/23
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
